c34564333444fa403ee191a6a4a853d1460bf8b6
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-03-12 19:44:23 -0400

    Creation of the FlickrAPI.swift

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Sergio de Almeida Brunacci on 2018-03-12.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrAPI {
+    
+}
